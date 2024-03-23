945eb1ff77e7c7d21b7c4505da39d8fd13709478
elliottcgriffin <elliott.griffin@foyer.ai>
2024-03-23 08:51:22 -0400

    Initial Commit

---
+++ RestMap/RestMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestMapApp.swift
+//  RestMap
+//
+//  Created by Elliott Griffin on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct RestMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
